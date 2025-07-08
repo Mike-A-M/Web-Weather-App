e38fae7683b39941c6501e5bfe83a8bd81dd7bb9
Mike-A-M <159654413+Mike-A-M@users.noreply.github.com>
2025-07-07 20:55:37 -0400

    Fetch Kitty Image

--- script/kitty.js
+++ script/kitty.js
@@ -1,12 +1,23 @@
-fetch('https://api.thecatapi.com/v1/images/search')
-    .then(response => response.json())
-    .then(imageArray => {
-        const container = document.getElementById('kitty'); // Assuming you have a container element
 
-        imageArray.forEach(imageObject => {
-        const img = document.createElement('img');
-        img.src = imageObject.url;
-        console.log(img.src);
-        container.appendChild(img);
-    });
-  });
+// Script to grab random image of kitty
+
+
+fetch('https://api.thecatapi.com/v1/images/search')
+
+    //parse response as a JSON
+
+    .then(response => response.json())
+
+    // Image array will now contain the list of parsed objects
+    .then(imageArray => {
+        
+            // target element in HTML Kitty
+            const container = document.getElementById('kitty');
+
+            imageArray.forEach(imageObject => {
+            const img = document.createElement('img');
+            img.src = imageObject.url;
+            console.log(img.src);
+            container.appendChild(img);
+   });
+});
